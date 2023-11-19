e7b4ee17b94298b127131a0bafcbcf94bb29afd4
arif123 <nashrullah2344@gmail.com>
2023-11-19 13:53:42 +0700

    fix

--- script.js
+++ script.js
@@ -1,53 +1,61 @@
-const apiKey = "f4d2480458b2058e14f2bac6d120f3a9";
-const apiUrl =
-    "http://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const { default: axios } = require('axios');
 
-const searchBox = document.querySelector('.search input');
-const searchBtn = document.querySelector('.search button');
-const weatherIcon = document.querySelector('.weather-icon');
+const apiKey = 'f4d2480458b2058e14f2bac6d120f3a9';
+const apiUrl = 'http://api.openweathermap.org/data/2.5/weather?units=metric&q=';
+
+let searchBox = document.querySelector('.search input');
+let searchBtn = document.querySelector('.search button');
+let weatherIcon = document.querySelector('.weather-icon');
 //input di bawah di butuhkan untuk membuat enter bisa di gunakan
 let input = document.querySelector('input');
 // Function to fetch weather data
 async function fetchWeatherData(city) {
-    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  try {
+    const response = await axios.get(apiUrl + city + `&appid=${apiKey}`);
+    const data = response.data;
 
-    if (response.status == 404) {
-        document.querySelector('.error').style.display = 'block';
-        document.querySelector('.weather').style.display = 'none';
+    if (response.status === 404) {
+      console.log('City not found');
+      // Handle the case where the city is not found
     } else {
-        var data = await response.json();
+      console.log('City:', data.name);
+      console.log('Temperature:', Math.round(data.main.temp) + '°C');
+      console.log('Humidity:', data.main.humidity + '%');
+      console.log('Wind speed:', data.wind.speed + ' km/h');
 
+      let weatherIcon;
 
-        document.querySelector('.city').innerHTML = data.name;
-        document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + "°c";
-        document.querySelector('.humidity').innerHTML = data.main.humidity + "%";
-        document.querySelector('.wind').innerHTML = data.wind.speed + " km/h";
+      switch (data.weather[0].main) {
+        case 'Clouds':
+          weatherIcon = 'images/clouds.png';
+          break;
+        case 'Clear':
+          weatherIcon = 'images/clear.png';
+          break;
+        case 'Rain':
+          weatherIcon = 'images/rain.png';
+          break;
+        case 'Drizzle':
+          weatherIcon = 'images/drizzle.png';
+          break;
+        default:
+          weatherIcon = 'images/mist.png';
+      }
 
-        if (data.weather[0].main == "Clouds") {
-            weatherIcon.src = 'images/clouds.png';
-        } else if (data.weather[0].main == "Clear") {
-            weatherIcon.src = 'images/clear.png';
-        } else if (data.weather[0].main == "Rain") {
-            weatherIcon.src = 'images/rain.png';
-        } else if (data.weather[0].main == "Drizzle") {
-            weatherIcon.src = 'images/drizzle.png';
-        } else {
-            (data.weather[0].main == "Mist")
-            weatherIcon.src = 'images/mist.png';
-        }
-
-        document.querySelector('.weather').style.display = 'block';
-        document.querySelector('.error').style.display = 'none';
+      console.log('Weather icon:', weatherIcon);
     }
+  } catch (error) {
+    console.error('Error fetching weather data:', error.message);
+  }
 }
-searchBtn.addEventListener("click", () => {
-    fetchWeatherData(searchBox.value);
-});
 
+searchBtn.addEventListener('click', () => {
+  fetchWeatherData(searchBox.value);
+});
 
 //cara agar tombol enter dapat di gunakan pada saat search
 input.addEventListener('keyup', (e) => {
-    if (e.keyCode === 13) {
-        fetchWeatherData(searchBox.value);
-    }
+  if (e.keyCode === 13) {
+    fetchWeatherData(searchBox.value);
+  }
 });
